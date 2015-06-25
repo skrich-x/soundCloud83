ac610329f28b71c6952deacccbe58737bdc60cdf
Kyle Richardson <s.k.richardson7@gmail.com>
2015-06-24 21:27:06 -0400

    Added component JS and template HBS

--- app/components/media-player.js
+++ app/components/media-player.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  init: function(){
+    SC.get('/tracks/', {limit: 20}, function(song) {
+			this.set('song', song);
+			console.log(song);
+		}.bind(this));
+  }
 });
